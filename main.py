b811cc89a34a5fcdf55fdff3956f39d373ddf4f6
jasurtojiboyev <tojiboyevj251@gmail.com>
2024-03-06 22:15:36 +0500

    OOP homework

---
+++ main.py
@@ -0,0 +1,66 @@
+class Bird1:
+    def __init__(self, name):
+        self.name = name
+
+    def sing(self):
+        return  f"{self.name}kuni 07:00 dan 13:00gacha maktabda bolaman"
+
+obj =  Bird1("1. Dushanba")
+print(obj.sing())
+class Bird2:
+    def __init__(self, name):
+        self.name = name
+
+    def sing(self):
+        return  f"{self.name}kuni 07:00 dan 13:00gacha maktabda bolaman"
+
+obj =  Bird2("2. Seshanba")
+print(obj.sing())
+class Bird3:
+    def __init__(self, name):
+        self.name = name
+
+    def sing(self):
+        return  f"{self.name}kuni 07:00 dan 13:00gacha maktabda bolaman"
+
+obj =  Bird3("3. Chorshanba")
+print(obj.sing())
+class Bird4:
+    def __init__(self, name):
+        self.name = name
+
+    def sing(self):
+        return  f"{self.name}kuni 07:00 dan 13:00gacha maktabda bolaman"
+
+obj =  Bird4("4. Payshanba")
+print(obj.sing())
+class Bird5:
+    def __init__(self, name):
+        self.name = name
+
+    def sing(self):
+        return  f"{self.name}kuni 07:00 dan 13:00gacha maktabda bolaman"
+
+obj =  Bird5("5. Juma")
+print(obj.sing())
+class Bird6:
+    def __init__(self, name):
+        self.name = name
+
+    def sing(self):
+        return  f"{self.name}kuni 07:00 dan 13:00gacha maktabda bolaman keyin qoshimcha dasturlash togaragigiga boraman"
+
+obj =  Bird6("6. Shanba")
+print(obj.sing())
+class Bird7:
+    def __init__(self, name):
+        self.name = name
+
+    def sing(self):
+        return  f"{self.name}kuni 07:00 dan 13:00gacha maktabda bolaman keyin qoshimcha dasturlash togaragigiga boraman"
+
+obj =  Bird7("7. Yakshanba")
+print(obj.sing())
+
+print("Qogan kunlari 13:00 dan 07 : 00 gacha boshman ! 24/7")
+
